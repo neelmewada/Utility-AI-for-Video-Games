be5f7fa2ecaa6d2bb15a3079d8a2872d57a9469f
Neel Mewada <neelrmewada@gmail.com>
2021-03-12 20:31:12 +0530

    Basic AI
    Basic AI

--- Assets/Scripts/AI/BotController.cs
+++ Assets/Scripts/AI/BotController.cs
@@ -1,31 +1,156 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 /// <summary>
-/// The class that controls the AI tanks
+/// The class that controls the AI tanks. It implements a Finite Statemachine with the Utility AI System
 /// </summary>
 public class BotController : TankController
 {
+    [Header("AI")]
+    public NavMeshAgent agent;
+    public float spotRadius = 10; // the max distance at which bot can spot the player
+    public float waypointSkipDist = 3;
+    public bool debug = false;
+
+    private List<Vector3> m_WaypointSelector = new List<Vector3>();
+    private List<Vector3> m_CurrentPath = new List<Vector3>();
+    private List<BotState> m_AllStates = new List<BotState>();
+
+    private TankController m_EnemyTarget;
+    private bool m_CanMove = true;
+
+    public TankController EnemyTarget { get => m_EnemyTarget; }
+    public BotState CurrentState { get; private set; }
+    public BotState PreviousState { get; private set; }
+    public float StoppingDistance { get; set; }
 
 
     protected override void Awake()
     {
         base.Awake();
-    }
+        ResetWaypoints();
 
-    protected override void FixedUpdate()
-    {
-        base.FixedUpdate();
+        m_AllStates.Add(new BotPatrolState());
+        m_AllStates.Add(new BotChaseState());
+
+        CurrentState = m_AllStates[0];
     }
 
     protected override void Update()
     {
         base.Update();
 
-        if (!CanUpdate())
+        if (!CanUpdate()) // If Update logic is disabled right now
+        {
+            MoveVector = Vector3.zero;
             return;
+        }
 
+        UpdateStateMachineScores(); // Update Utility scores
 
+        CurrentState.UpdateState(this); // Call Update logic for current state
+
+        if (!m_CanMove || m_CurrentPath.Count == 0)
+        {
+            MoveVector = Vector3.zero;
+            return;
+        }
+
+        if (Vector3.Distance(transform.position, m_CurrentPath[0]) < StoppingDistance)
+        {
+            m_CurrentPath.RemoveAt(0);
+        }
+
+        if (m_CurrentPath.Count == 0)
+        {
+            MoveVector = Vector3.zero;
+            return;
+        }
+
+        Vector3 dir = m_CurrentPath[0] - transform.position;
+        MoveVector = dir.normalized;
+    }
+
+    /// <summary>
+    /// This function implements the Utility AI by calculating scores of each viable state and sets the current state
+    /// </summary>
+    protected void UpdateStateMachineScores()
+    {
+        var player = GameManager.Instance.Player;
+
+        float[] stateScores = new float[m_AllStates.Count]; // [0] = patrol, [1] = chase, [2] = shoot, ...
+        int highScoringState = 0;
+        float highestRunningScore = 0;
+
+        for (int i = 0; i < m_AllStates.Count; i++)
+        {
+            stateScores[i] = m_AllStates[i].CalculateScore(this);
+            if (stateScores[i] > highestRunningScore)
+            {
+                highestRunningScore = stateScores[i];
+                highScoringState = i;
+            }
+        }
+
+        if (highestRunningScore == -1) return; // -1 means state isn't available right now
+
+        BotState state = m_AllStates[highScoringState];
+        if (CurrentState != state) // if state needs to be changed
+        {
+            PreviousState = CurrentState;
+            CurrentState = state;
+            CurrentState.OnStateChanged(this, PreviousState);
+        }
+    }
+
+    // Resets the waypoint selector list. This list is used to cycle through waypoints
+    private void ResetWaypoints()
+    {
+        m_WaypointSelector.Clear();
+
+        for (int i = 0; i < GameManager.Instance.waypointsParent.childCount; i++)
+        {
+            m_WaypointSelector.Add(GameManager.Instance.waypointsParent.GetChild(i).position);
+        }
+    }
+
+    public Vector3 GetRandomWaypoint()
+    {
+        if (m_WaypointSelector.Count == 0)
+            ResetWaypoints();
+
+        int wpIndex = Random.Range(0, m_WaypointSelector.Count);
+        Vector3 waypoint = m_WaypointSelector[wpIndex];
+        m_WaypointSelector.RemoveAt(wpIndex);
+
+        return waypoint;
+    }
+
+    public bool SetPathDestination(Vector3 destination)
+    {
+        m_CurrentPath.Clear();
+
+        NavMeshPath path = new NavMeshPath();
+        if (!NavMesh.CalculatePath(transform.position, destination, -1, path))
+        {
+            Debug.LogError("FATAL ERROR: The destination at position " + destination + " is NOT reachable!");
+            return false;
+        }
+
+        for (int i = 0; i < path.corners.Length; i++)
+        {
+            if (Vector3.Distance(path.corners[i], transform.position) > 3f)
+                m_CurrentPath.Add(path.corners[i]);
+        }
+
+        return m_CurrentPath.Count > 0;
+    }
+
+    public bool IsPathEmpty()
+    {
+        return m_CurrentPath.Count == 0;
     }
 }
---
+++ Assets/Scripts/AI/BotState.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public abstract class BotState
+{
+
+    public abstract float CalculateScore(BotController bot);
+
+    /// <summary>
+    /// Called when state is changed from previousState to this state
+    /// </summary>
+    public abstract void OnStateChanged(BotController bot, BotState previousState);
+
+    /// <summary>
+    /// Update logic for current state
+    /// </summary>
+    public abstract void UpdateState(BotController bot);
+}
+
+public class BotPatrolState : BotState
+{
+
+    public override float CalculateScore(BotController bot)
+    {
+        return 10;
+    }
+
+    public override void OnStateChanged(BotController bot, BotState previousState)
+    {
+        Vector3 wp = bot.GetRandomWaypoint();
+
+        bot.SetPathDestination(wp);
+        bot.StoppingDistance = bot.waypointSkipDist;
+    }
+
+    public override void UpdateState(BotController bot)
+    {
+        if (bot.IsPathEmpty()) // if waypoint reached, reset to new waypoint
+        {
+            Vector3 newWaypoint = bot.GetRandomWaypoint();
+
+            bot.SetPathDestination(newWaypoint);
+        }
+    }
+}
+
+public class BotChaseState : BotState
+{
+    private Vector3 m_LastPlayerPos;
+
+    public override float CalculateScore(BotController bot)
+    {
+        var player = GameManager.Instance.Player;
+        if (player == null || player.IsDead) return 0;
+
+        float dist = Vector3.Distance(bot.transform.position, player.transform.position);
+        float score = dist > bot.spotRadius ? 0 : 20;
+
+        return score;
+    }
+
+    public override void OnStateChanged(BotController bot, BotState previousState)
+    {
+        bot.StoppingDistance = 10;
+    }
+
+    public override void UpdateState(BotController bot)
+    {
+        var player = GameManager.Instance.Player;
+        if (player == null || player.IsDead) return;
+
+
+        if (Vector3.Distance(player.transform.position, bot.transform.position) > 10f)
+        {
+            bot.SetPathDestination(player.transform.position);
+        }
+
+        m_LastPlayerPos = player.transform.position;
+    }
+}
